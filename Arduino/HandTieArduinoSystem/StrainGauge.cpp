c73a10db4f5b73a5b16dacc6f74ef899f9ad463d
timowang1991 <timowang1991@yahoo.com.tw>
2015-02-01 22:46:07 +0800

    In HandTieArduinoSystem, new data members, "targetValNoAmp" and "targetValWithAmp" are added

--- Arduino/HandTieArduinoSystem/StrainGauge.cpp
+++ Arduino/HandTieArduinoSystem/StrainGauge.cpp
@@ -4,9 +4,12 @@ StrainGauge::StrainGauge(){
 
 }
 
-StrainGauge::StrainGauge(uint8_t ampPotPos, uint8_t bridgePotPos){
+StrainGauge::StrainGauge(uint8_t ampPotPos, uint8_t bridgePotPos, uint8_t targetValNoAmp,
+                         uint8_t targetValWithAmp){
    this->ampPotPos = ampPotPos;
    this->bridgePotPos = bridgePotPos;
+   this->targetValNoAmp = targetValNoAmp;
+   this->targetValWithAmp = targetValWithAmp;
 }
 
 StrainGauge::~StrainGauge(){
@@ -27,4 +30,20 @@ void StrainGauge::setBridgePotPos(uint8_t bridgePotPos){
 
 uint8_t StrainGauge::getBridgePotPos(){
    return bridgePotPos;
+}
+
+void StrainGauge::setTargetValNoAmp(uint8_t targetVal){
+   this->targetValNoAmp = targetVal;
+}
+
+uint8_t StrainGauge::getTargetValNoAmp(){
+   return targetValNoAmp;
+}
+
+void StrainGauge::setTargetValWithAmp(uint8_t targetVal){
+   this->targetValWithAmp = targetVal;
+}
+
+uint8_t StrainGauge::getTargetValWithAmp(){
+   return targetValWithAmp;
 }
--- Arduino/HandTieArduinoSystem/StrainGauge.h
+++ Arduino/HandTieArduinoSystem/StrainGauge.h
@@ -7,7 +7,7 @@ class StrainGauge
 {
 public:
    StrainGauge();
-   StrainGauge(uint8_t, uint8_t);
+   StrainGauge(uint8_t, uint8_t, uint8_t, uint8_t);
    ~StrainGauge();
 
    void setAmpPotPos(uint8_t);
@@ -16,9 +16,18 @@ public:
    void setBridgePotPos(uint8_t);
    uint8_t getBridgePotPos();
 
+   void setTargetValNoAmp(uint8_t);
+   uint8_t getTargetValNoAmp();
+
+   void setTargetValWithAmp(uint8_t);
+   uint8_t getTargetValWithAmp();
+
 private:
    uint8_t ampPotPos;
    uint8_t bridgePotPos;
+
+   uint8_t targetValNoAmp;
+   uint8_t targetValWithAmp;
 };
 
 #endif   //StrainGauge_h
