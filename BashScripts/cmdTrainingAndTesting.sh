c1cf9afa3e9b7a60b8ba8d761b62704a85ab0329
Tsukino KanTei <r03944003@ntu.edu.tw>
2015-03-21 00:37:07 +0800

    add scripts for training and testing using libsvm or linear and output accuracy and result to file

---
+++ BashScripts/cmdTrainingAndTesting.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+#-- config --
+
+usingTool='linear'
+#usingTool='nonLinear'
+
+liblinearPath='/Users/lab430/Documents/SVM/liblinear-1.96'
+libSVMPath='/Users/lab430/Documents/SVM/libsvm-3.20'
+
+#--
+
+if [ $# == 0 ]; then
+  echo "usage:$0 inputDataFolderPath [outputResultFolderPath] [-v number]"
+  exit
+fi
+
+inputDataFolderPath=$1
+outputResultFolderPath=$2
+
+
+if [ $usingTool == 'linear' ]; then
+  train=$liblinearPath'/train'
+  predict=$liblinearPath'/predict'
+else
+  train=$libSVMPath'/svm-train'
+  predict=$libSVMPath'/svm-predict'
+fi
+
+
+#-- train and then test
+cd $inputDataFolderPath
+allTrainingData=`ls . | grep 'training'`
+allTestingData=`ls . | grep 'testing'`
+#echo $allTestingData
+#trainingOpts=''
+#testingOpts=''
+
+accuracyFile=`date +"%Y-%m-%d_%k-%M-%S"`'.accuracy'
+
+for trainingFile in $allTrainingData; do
+  modelFile=${trainingFile%%.*}'.model'
+  $train -q $trainingFile $modelFile
+  fileID=${trainingFile%%_*} #use this fileID to find its complementary testing file
+  #echo $fileID
+  for testingFile in $allTestingData; do
+    if [[ $testingFile =~ ^"$fileID"_.* ]]; then #matching the first one
+      #echo $testingFile
+      pureTestFileName=${testingFile%%.*}
+      resultFile=$pureTestFileName'.result'
+      #echo $resultFile >>"$accuracyFile"
+      #$predict $testingFile $modelFile $resultFile >>"$accuracyFile" 2>&1
+      #$predict $testingFile $modelFile $resultFile
+      rowNums=`echo $resultFile | cut -d'_' -f4`
+      $predict $testingFile $modelFile $resultFile | awk -v var="$rowNums" '{print var','$3}' >>"$accuracyFile"
+      break
+    fi
+  done
+done
+if [ -d $outputResultFolderPath ] && [ "$outputResultFolderPath" != "$inputDataFolderPath" ]; then 
+  mv *.accuracy *.result *.model $outputResultFolderPath
+fi
+
+#if [ $3 == '-v' ]; then
+#-- do cross validation
+#numFold=$4.
+
+
+#fi
+
+
+
