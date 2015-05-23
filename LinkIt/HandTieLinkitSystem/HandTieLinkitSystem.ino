18bfe3666d2ca37952feaafc58d3be5c512232aa
王權 <timowang1991@gmail.com>
2015-05-23 14:51:47 +0800

    HandTieLinkitSystem initializes data type in setup()

--- LinkIt/HandTieLinkitSystem/HandTieLinkitSystem.ino
+++ LinkIt/HandTieLinkitSystem/HandTieLinkitSystem.ino
@@ -3,22 +3,27 @@
 #include "ParserWithAction.h"
 #include "RecordButton.h"
 
-SGManager sgManager;
-ParserWithAction parser(&sgManager);
-RecordButton recordButton;
+SGManager * sgManager;
+ParserWithAction * parser;
+RecordButton * recordButton;
 
 void setup(){
    Serial.begin(38400);
-   sgManager.allCalibrationAtConstAmp();
+   
+   sgManager = new SGManager();
+   parser = new ParserWithAction(sgManager);
+   recordButton = new RecordButton();
+
+   sgManager->allCalibrationAtConstAmp();
 }
 
 void loop(){
-   sgManager.serialPrint();
-   recordButton.checkClick();
+   sgManager->serialPrint();
+   recordButton->checkClick();
 }
 
 void serialEvent(){
    while(Serial.available()){
-      parser.parse();
+      parser->parse();
    }
 }
