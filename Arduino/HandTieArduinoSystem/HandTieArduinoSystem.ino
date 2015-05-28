0a37f5a59265d6a4c7d59881415bcf084c77b53f
王權 <timowang1991@gmail.com>
2015-05-28 10:49:05 +0800

    accelerometer ADXL345 works well, but serial and calibration need fix

--- Arduino/HandTieArduinoSystem/HandTieArduinoSystem.ino
+++ Arduino/HandTieArduinoSystem/HandTieArduinoSystem.ino
@@ -1,24 +1,31 @@
 #include <SPI.h>
+#include <Wire.h>
 #include "SGManager.h"
 #include "ParserWithAction.h"
 #include "RecordButton.h"
+#include "ADXL345.h"
 
 SGManager sgManager;
 ParserWithAction parser(&sgManager);
 RecordButton recordButton;
+ADXL345 * accel;
 
 void setup(){
    Serial.begin(38400);
    sgManager.allCalibrationAtConstAmp();
+   accel = new ADXL345();
 }
 
 void loop(){
    sgManager.serialPrint();
+   accel->serialPrintRaw();
    recordButton.checkClick();
+   
+   Serial.println();
 }
 
 void serialEvent(){
    while(Serial.available()){
-      parser.parse();
+     parser.parse();
    }
 }
